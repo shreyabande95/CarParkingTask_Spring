4b4b982043fae021b898f541175e3791869e1ed4
Near <near@Nears-MacBook-Pro.local>
2021-02-28 21:38:44 +0530

    Adding test cases

--- CarParkingTask_Spring/SpringMongodb/src/test/java/spring/SpringwithmongodbApplicationTests.java
+++ CarParkingTask_Spring/SpringMongodb/src/test/java/spring/SpringwithmongodbApplicationTests.java
@@ -1,49 +1,61 @@
 package spring;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import spring.client.MongodbClient;
+import spring.controller.ControllerClass;
 import spring.dto.Car;
 import spring.repository.CarRepository;
 
 import javax.annotation.security.RunAs;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
-@RunAs("Spring")
+
 @SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@AutoConfigureMockMvc
 class SpringwithmongodbApplicationTests {
+
 	@Autowired
-	MongodbClient mongodb;
+	public MockMvc mvc;
 
-	@MockBean
-	CarRepository carRepo;
-	@Test
-	public void addCarTest(){
-		Car car=new Car(1,1,"blue",0);
-		when(carRepo.save(car)).thenReturn(car);
-		assertEquals("Successfully added Car with Registration Number:1",mongodb.addCar(car));
+	@Autowired
+	public ControllerClass controllerClass;
 
-	}
+
 	@Test
-	public void getSlotByColorTest(){
-		Car car=new Car(2,2,"red",0);
-		when(carRepo.findById(2)).thenReturn(java.util.Optional.of(car));
-		assertEquals(0,car.allotedSlot);
-	}
-	@Test
-	public void getSlotByRegNoTest(){
-		Car car=new Car(2,2,"red",0);
-		when(carRepo.findById(2)).thenReturn(java.util.Optional.of(car));
-		assertEquals(0,car.allotedSlot);
+	public void addCarTest() throws Exception{
+		mvc.perform(MockMvcRequestBuilders.get("/v1/cars/post_car/932/red")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().string("Successfully added Car with Registration Number:932"));
+
 	}
 
 	@Test
-	public void getRegNoByColorTest(){
-		Car car=new Car(2,2,"red",0);
-		when(carRepo.findById(2)).thenReturn(java.util.Optional.of(car));
-		assertEquals(2,car.registrationNo);
+	public void getRegNoByColorTest() throws Exception{
+		//mvc.perform(MockMvcRequestBuilders.get("/v1/cars/post_car/KA0932/red")).andReturn();
+		mvc.perform(MockMvcRequestBuilders.get("/v1/cars/search/registration_number/red")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().string("Registration Number:2 Registration Number:3 Registration Number:4 Registration Number:932 "));
+	}
+
+	@Test
+	public void getSlotByRegNoTest() throws Exception{
+		mvc.perform(MockMvcRequestBuilders.get("/v1/cars/search/slot_by_regno/932")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().string("[Floor:1 Slot:1] "));
+	}
+
+	@Test
+	public void getSlotByColorTest() throws Exception{
+		mvc.perform(MockMvcRequestBuilders.get("/v1/cars/search/slot/red")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().string("[Floor:1 Slot:1] [Floor:1 Slot:1] [Floor:1 Slot:1] [Floor:1 Slot:1] "));
+	}
+
+	@Test
+	public void deleteTest() throws Exception{
+		mvc.perform(MockMvcRequestBuilders.get("/v1/cars/delete/9")).andExpect(MockMvcResultMatchers.status().isOk());
 	}
 	@Test
 	void contextLoads() {
